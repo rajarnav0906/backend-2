95631ef7108311928f10947e255349aeb7977655
rajarnav0906 <arnavraj2701@gmail.com>
2024-06-20 20:45:45 +0530

    utils file handling done

--- src/app.js
+++ src/app.js
@@ -45,3 +45,6 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 export { app }
+
+
+
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,31 @@
+//using .then().catch()
+
+    const asyncHandler = (requestHandler) => {
+        (err, req, res, next) => {
+            Promise
+                .resolve((requestHandler(err, req, res, next)))
+                .catch((error) => next(error))
+        }
+    }
+
+    export { asyncHandler }
+
+
+
+
+
+
+//using try and catch
+
+    // const asyncHandler = (requestHandler) => {
+    //     async (err, req, res, next) => {
+    //         try {
+    //             await requestHandler(err, req, res, next);
+    //         } catch (error) {
+    //             res.status(error.code || 500).json({
+    //                 success: false,
+    //                 message : error.message
+    //             })
+    //         }
+    //     }
+    // }
